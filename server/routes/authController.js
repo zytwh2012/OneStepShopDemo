e9198d830738462946519605038ff4decf97e6b6
Yuntian Zhang <zytwh2012@gmail.com>
2019-08-15 23:20:01 -0400

    auth back end

--- server/routes/authController.js
+++ server/routes/authController.js
@@ -3,22 +3,156 @@ const authRouter = express.Router()
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
+const awaitHandlerFactory = middleware => {
+    return async (req, res, next) => {
+        try {
+            await middleware(req, res, next)
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
 // api/authentication/signup
 authRouter.post('/signup', (req, res) => {
     let data = req.body
     let newUser = new User(data.user)
     newUser.save((error, data) => {
-      if (error) {
-        logger.info('/signup => 400 ', error)
-        res.status(400).send({ message: error, query: 'signUp', status: 'unsucessful' })
-      } else {
-        let user = {
-          username: data.username,
-          id: data.id,
-          createDate: data.createDate
+        if (error) {
+            console.log('/signup  400 ', error)
+            res.status(400).send({ message: error, query: 'signUp', status: 'unsucessful' })
+        } else {
+            let user = {
+                username: data.username,
+                id: data.id,
+            }
+            console.log('/signup  200 ')
+            res.status(200).send({ user: user, query: 'signUp', status: 'sucessful' })
         }
-        logger.info('/signup => 200 ')
-        res.status(200).send({ user: user, query: 'signUp', status: 'sucessful' })
-      }
     })
-  })
+})
+
+
+// api/authentication/signin
+authRouter.post('/signin', awaitHandlerFactory(async (req, res) => {
+    let data = req.body
+    let signinUser = new User(data.user)
+    await User.findOne({ username: signinUser.username }, (error, user) => {
+        if (error) {
+            console.log('/signin  520 ', error)
+            res.status(520).send({ 
+                message: error, 
+                query: 'signIn', 
+                status: 'unsucessful' 
+            })
+            return
+        }
+        if (!user || user === undefined) {
+            console.log('/signin  400 ', 'password or username is not correct')
+            return res.status(400).send({
+                message: 'password/username is not correct',
+                query: 'signIn',
+                status: 'unsucessful'
+            })
+        }
+        // verify password
+        User.validatePassword(user.password, signinUser.password, async (error, isMatch) => {
+            if (error) {
+                console.log('/signin 400 ', error)
+                res.status(400).send({ 
+                    message: error, 
+                    query: 'signIn', 
+                    status: 'unsucessful' })
+            }
+            if (isMatch) {
+                try {
+                    // token exists check if token is expired
+                    jwt.verify(user.token, 'biubiu')
+                    await User.findById(user.id, '-password', (err, data) => {
+                        if (err) return res.sendStatus(520)
+                        if (data) {
+                            // lastModified
+                            data.modifyDate = new Date()
+                            data.save()
+                            console.log('api/signin 200 ')
+                            return res.status(200).send({ 
+                                user: data, 
+                                query: 'signIn', 
+                                status: 'sucessful' })
+                        }
+                    })
+                } catch (error) {
+                    // token is expire generate token
+                    if (error.name === 'TokenExpiredError' || error.name === 'JsonWebTokenError') {
+                        const usrJson = User.toAuthJSON(user, true)
+                        await User.findById(user.id, '-password', (err, data) => {
+                            if (err) return res.sendStatus(520)
+                            if (data) {
+                                data.token = usrJson.token
+                                data.echart = dailyLogin(user)
+                                data.save()
+                                console.log('api/signin 200 token refresh')
+                                res.status(200).send({ 
+                                    user: usrJson, 
+                                    query: 'signIn', 
+                                    status: 'sucessful' })
+                            }
+                        })
+                    } else {
+                        console.log('/signin 400 ', error)
+                        res.status(400).send({ 
+                            message: error, 
+                            query: 'signIn', 
+                            status: 'unsucessful' })
+                    }
+                }
+            } else {
+                console.log('/signin 403 ', error)
+                res.status(403).send({
+                    query: 'signIn',
+                    status: 'unsucessful',
+                    message: 'username/password is not correct'
+                })
+            }
+        })
+    })
+}))
+
+
+// api/authentication/signout
+authRouter.delete('/signout', verifyToken, (req, res) => {
+    User.findByIdAndUpdate(req.userid, { 'token': '' }, { useFindAndModify: false }, (error) => {
+        if (error) {
+            console.log('/signout 520', error)
+            res.status(520).send({ query: 'signOut', message: error })
+        } else {
+            console.log('/signout 200')
+            res.status(200).send({ query: 'signOut', message: "sucessful" })
+        }
+    })
+})
+
+
+function verifyToken(req, res, next) {
+    // verify the Json Token
+    if (!req.headers.authorization) {
+        console.log('verifyToken 401 ', 'Unauthorized request')
+        return res.status(401).send('Unauthorized request')
+    }
+    let token = req.headers.authorization.split(' ')[1]
+    if (token === 'null') {
+        console.log('verifyToken  401 ', 'Unauthorized request')
+        return res.status(401).send('Unauthorized request')
+    }
+    try {
+        let payload = jwt.verify(token, 'biubiu')
+        req.username = payload.username
+        req.userid = payload.id
+    } catch (error) {
+        console.log('verifyToken 401 ', 'Unauthorized request')
+        return res.status(401).send({ error: error })
+    }
+    next()
+}
+
+module.exports = authRouter
--- server/server.js
+++ server/server.js
@@ -2,10 +2,52 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const timeout = require('connect-timeout')
+const path = require('path')
+const authApi = require('./routes/authController')
+const database = 'mongodb+srv://sososos19:98765432@cluster0-wfs9h.mongodb.net/test?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 3000
 
-app.use(bodyParser.json({ limit: '5mb' }))
-
+// init connetction to remote database
+mongoose.connect(database, { useNewUrlParser: true }, error => {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log('connected to database')
+  }
+})
 app.listen(PORT, function () {
-    console.log('server running on localhost' + PORT)
-  })
+  console.log('server running on localhost' + PORT)
+})
+
+// angualr static file
+app.use(express.static(path.join(__dirname, './')))
+app.use(timeout('10s'))
+app.use(bodyParser.urlencoded({ extended: true, limit: '5mb' }))
+app.use(bodyParser.json({ limit: '5mb' }))
+app.use(haltOnTimedout)
+
+// add api controllers
+app.use('/api/authentication', authApi)
+
+app.all('/*', function (req, res, next) {
+  // Just send the index.html for other files to support HTML5Mode
+  res.sendFile('index.html', { root: __dirname })
+})
+
+// https://www.npmjs.com/package/connect-timeout
+function haltOnTimedout (error, req, res, next) {
+  if (error) {
+    console.log(error)
+    if (error.status) {
+      res.status(error.status).send({ message: error.message })
+      return
+    }
+  }
+  if (!req.timedout) {
+    console.log(JSON.stringify(req.route))
+    next()
+  } else {
+    res.status(508).send({ message: 'timeout!' })
+  }
+}
